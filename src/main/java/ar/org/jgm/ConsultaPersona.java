5c3bde4b2d5de599fb27f0163cebd754a168ca2d
Federico J. Botti <fbotti@gmail.com>
2014-03-04 05:20:37 -0300

    MuleESB REST API with MongoDB Example

---
+++ src/main/java/ar/org/jgm/ConsultaPersona.java
@@ -0,0 +1,40 @@
+package ar.org.jgm;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+
+@Path("/")
+public class ConsultaPersona {
+	
+
+	@GET
+	@Produces(MediaType.APPLICATION_JSON)
+	@Path("persona/cuil/{cuil}")
+	public Map<String, Object> getPersonaCuilJSON(@PathParam("cuil") String cuil){
+		Map<String, Object> query = new HashMap<String, Object>();  
+		query.put("cuil", cuil);    
+		return query; 
+	}
+	
+	@GET
+	@Produces(MediaType.APPLICATION_JSON)
+	@Path("persona/dni/{cuil}")
+	public Map<String, Object> getDemoPersonaJSON(@PathParam("cuil") String cuil){
+		Map<String, Object> query = new HashMap<String, Object>();  
+		query.put("cuil", cuil);    
+		return query; 
+	}
+	
+	@GET
+	@Produces(MediaType.APPLICATION_JSON)
+	@Path("personas")
+	public String getPersonasJSON(){    
+		return null; 
+	}
+}
