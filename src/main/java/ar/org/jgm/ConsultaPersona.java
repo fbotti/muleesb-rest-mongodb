c07991a02755fa6f43f8d20f42cb9af48bb5e1d4
Federico J. Botti <fbotti@gmail.com>
2014-03-05 00:09:10 -0300

    Gitignore changes

--- src/main/java/ar/org/jgm/ConsultaPersona.java
+++ src/main/java/ar/org/jgm/ConsultaPersona.java
@@ -7,7 +7,10 @@ import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
+import javax.ws.rs.QueryParam;
 import javax.ws.rs.core.MediaType;
+
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Path("/")
 public class ConsultaPersona {
@@ -18,7 +21,7 @@ public class ConsultaPersona {
 	@Path("persona/cuil/{cuil}")
 	public Map<String, Object> getPersonaCuilJSON(@PathParam("cuil") String cuil){
 		Map<String, Object> query = new HashMap<String, Object>();  
-		query.put("cuil", cuil);    
+		query.put("cuil", cuil);
 		return query; 
 	}
 	
